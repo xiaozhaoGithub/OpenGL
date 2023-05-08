60145ffa1ec080b695ed9b9fd2b08abcba56fb3b
zhaogangming <1085080350@qq.com>
2023-05-08 22:12:49 +0800

    build(global): 增加项目构建相关脚本文件
    1.增加cmake管理项目，支持64位下Debug、Release配置
    2.增加cmake转vs2017 64位工程脚本
    3.增加生成后拷贝库、资源脚本
    4.删除模型文件压缩包

--- OpenGLDemo/Project/PostBuild.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-echo %cd%
-xcopy "third_lib\Assimp\lib\assimp-vc141-mtd.dll" "..\ExeFile\Debug_64" /S /Y
-
-pause
---
+++ OpenGLDemo/Project/PostBuild_64.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo %cd%
+
+REM Debug
+if exist ..\ExeFile\Debug_64\ (
+	xcopy "third_lib\Assimp\lib\assimp-vc141-mtd.dll" "..\ExeFile\Debug_64\" /Y /D
+	xcopy "skin" "..\ExeFile\%Debug_64\skin\" /Y /D /E
+	xcopy "ShaderProgram" "..\ExeFile\Debug_64\ShaderProgram\" /Y /D /E
+)
+
+REM Release
+if exist ..\ExeFile\Release_64\ (
+	xcopy "third_lib\Assimp\lib\assimp-vc141-mtd.dll" "..\ExeFile\Release_64\" /Y /D
+	xcopy "skin" "..\ExeFile\Release_64\skin\" /Y /D /E
+	xcopy "ShaderProgram" "..\ExeFile\Release_64\ShaderProgram\" /Y /D /E
+)
+
+pause
+
